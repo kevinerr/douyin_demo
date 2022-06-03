88ecb27677d34ef349a63c7bb5c234d348cd8c1b
Sensorjang <515310624@qq.com>
2022-06-03 23:04:21 +0800

    debug feed time bug

--- controller/feed.go
+++ controller/feed.go
@@ -1,6 +1,7 @@
 package controller
 
 import (
+	"fmt"
 	"github.com/RaymondCode/simple-demo/service"
 	"github.com/gin-gonic/gin"
 	"net/http"
@@ -16,7 +17,9 @@ type FeedResponse struct {
 
 func Feed(c *gin.Context) {
 	//可选参数，限制返回视频的最新投稿时间戳，精确到秒，不填表示当前时间
+	fmt.Println("请求feed的携带时间", c.Query("latest_time"))
 	latestTime := c.DefaultQuery("latest_time", strconv.Itoa(int(time.Now().Unix())))
+	latestTime = latestTime[0:10]
 	token := c.Query("token")
 	var feedService service.FeedService
 	res := feedService.VideoList(latestTime, token)
